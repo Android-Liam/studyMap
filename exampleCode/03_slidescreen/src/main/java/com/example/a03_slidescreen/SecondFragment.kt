1f81a8d1d8cdbe64d6de7ddb2a834ed7b4a90a3b
Liam <koreadevliam@gmail.com>
2023-08-27 01:45:29 +0900

    add Slide Screen Module

---
+++ exampleCode/03_slidescreen/src/main/java/com/example/a03_slidescreen/SecondFragment.kt
@@ -0,0 +1,31 @@
+package com.example.a03_slidescreen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.setOnClickListener {
+            navigateToFirstFragment()
+        }
+    }
+
+    private fun navigateToFirstFragment() {
+        val navController = findNavController()
+        navController.navigate(R.id.action_secondFragment_to_firstFragment)
+    }
+}
